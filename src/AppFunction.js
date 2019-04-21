adffc5b36414daead2a463eb73369df6b05d22b4
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2019-04-21 18:21:39 +0200

    Cleaning up Listeners without a supportive api.

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
+
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
   const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(
+    initialLocationState
+  );
+  let mounted = true;
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1);
@@ -28,15 +38,29 @@ const App = () => {
     setStatus(false);
   };
 
+  const handleGeoLocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
+
   useEffect(() => {
     document.title = `You have clicked ${count} times.`;
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('online', handleOnline);
     window.addEventListener('offline', handleOffline);
+    navigator.geolocation.getCurrentPosition(handleGeoLocation);
+    const watchID = navigator.geolocation.watchPosition(handleGeoLocation);
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('online', handleOnline);
       window.removeEventListener('offline', handleOffline);
+      navigator.geolocation.clearWatch(watchID);
+      mounted = false;
     };
   }, [count]);
 
@@ -68,6 +92,10 @@ const App = () => {
       <p>
         You are <strong>{status ? 'online' : 'offline'}</strong>
       </p>
+      <h2>Geolocation</h2>
+      <p>Latitude is {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Your speed is {speed ? speed : '0'}</p>
     </div>
   );
 };
